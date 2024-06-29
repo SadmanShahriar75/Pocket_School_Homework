9d6744285b37ec9dc10080b39be02eed35223969
SadmanShahriar75 <sadmanshahriar7576@gmail.com>
2024-06-29 12:31:03 +0600

    hey

--- Array/problems_5.py
+++ Array/problems_5.py
@@ -1,7 +1,7 @@
 # Find min of two number 12,3 what is the min ?
 
 def minofArray():
-    arr = [12,3]
+    arr = [12,3,]
     min_element = arr[0]
     for i in range(len(arr)):
         if arr[i] < min_element:
